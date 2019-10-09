57980c950bfbd2223b434c9585af73fc6f9fc305
yefran2015 <yefran2015@gmail.com>
2019-10-09 15:27:07 -0500

    Delete users_info_table_creation_command.sql

--- users_info_table_creation_command.sql
+++
@@ -1,3 +0,0 @@
-create table user_info(user_id int(11) UNIQUE, lastName varchar(100) not null, firstName varchar(100) not null, 
-address varchar(200) not null, city varchar(100) not null, state char(2) not null, zip char(5) not null,
-email varchar(150) not null, primary key(user_id), foreign key(user_id) references users(id));
