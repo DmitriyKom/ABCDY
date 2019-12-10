0cd1394205197b55d81dc477c5cf997fd48d2317
yefran2015 <yefran2015@gmail.com>
2019-12-10 03:46:10 -0600

    all design issues are solved

--- wrapper/Header.php
+++ wrapper/Header.php
@@ -1,32 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
-    <title>ABCDY</title>
-    <style>
-        html{
-            height:100%;
-        }
-        #Banner{
+    
+
+    <link rel="stylesheet" href="./design/bootstrap.css">
+    <style type="text/css">
+ 			#Banner{
             width: 100%;
             height: 130px;
             background-color: whitesmoke;
             display:block;
-            position: fixed;
+       
             float: left;
             margin: 0;
             padding: 0;
         }
-        #Main{
-            float: right;
-            clear: right;
-            position: fixed;
-            margin-top: 170px;
-            width: 100%;
-            height: 100%;
-            margin-left: 40%;
-        }
-        #Logo{
+         #Logo{
             float:left;
             width:100px;
             height: 100px;
@@ -36,12 +25,71 @@
             font-size: 7em;
             font-family: "Brush Script MT";
         }
-    
-    </style>
-</head>
-<body>
-<div id="Banner">
-    <div id="Logo">ABCDY</div>
-</div>
+        body {
+            font: 14px sans-serif;
+            text-align: center;
+            background-image: url("wrapper/Background.jpeg");
+            background-repeat: no-repeat;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
 
-<div id="Main">
+        table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 95%;
+        }
+
+        td, th {
+            border: 1px solid #dddddd;
+            text-align: center;
+            padding: 8px;
+            color: black;
+           
+        }
+
+        tr:nth-child(even) {
+            background-color: #dddddd;
+				opacity: 0.7;
+        }
+        tr:nth-child(odd) {
+            background-color: white;
+				opacity: 0.7;
+        }
+        .page-header{
+            background-color: whitesmoke;
+            text-decoration: none;
+            opacity: 0.5;
+        }
+
+        a:hover{
+            text-decoration: none;
+            background-color: lightblue;
+        }
+        .row{
+            color:#000000;
+            text-decoration: none;
+            font-weight:  bold;
+            font-size: 2.5em;
+            text-align: center;
+            vertical-align: middle;
+            padding-bottom: -100px;
+            display: block;
+        }
+		  #wrapper{
+            background-color: white;
+            margin-left: auto;
+  				margin-right: auto;
+        }
+        
+        .wrapper{ 
+        		width: 350px; 
+        		padding: 20px; 
+        		background-color: white; 
+        		margin-left: auto;
+  				margin-right: auto;
+  			
+  			}
+           
+    </style>
